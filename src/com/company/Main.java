a0be6c8ca1285ad900ed2da39345c86833c0dae6
Bruno Martins <brunoemartinslx@gmail.com>
2021-12-06 22:03:36 -0500

    Constructor, Setters and Getters

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+      Caneta c1 = new Caneta("NIC", "Amarelo", 0.2f);
+      c1.status();
+
+      Caneta c2 = new Caneta("BIC", "Laranja", 07f);
+      c2.status();
+    }
+}
